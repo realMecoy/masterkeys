9170c637789254e5a0aa51aedb89745eb6cd3735
realMecoy <realmecoy@gmail.com>
2024-12-15 10:00:41 +0200

    Updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   // ...
   integrations: [tailwind()],
-  site: "https://github.com/realMecoy/masterkeys",
+  site: "https://realmecoy.github.io/masterkeys/",
   base: "masterkeys",
 });
