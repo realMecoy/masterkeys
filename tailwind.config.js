c3535fe1c231ea038da40c0cb533b58b61fa5efb
realMecoy <realmecoy@gmail.com>
2024-12-15 09:48:33 +0200

    Added missing files and folders

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        lobster: ["Lobster", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
